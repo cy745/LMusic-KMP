3d4253f1891de471ccc2ab49b4fc4631c2d7da0d
lalilu <1248393538@qq.com>
2025-08-25 08:38:47 +0800

    refactor: 移除 Koin 日志记录配置
    移除了 MainApplication 中的 androidLogger 配置项。这项配置主要用于在开发过程中记录 Koin 相关的日志，现在移除可能是为了减少生产环境中的日志输出，提高应用性能或减少日志文件大小。

--- composeApp/src/androidMain/kotlin/com/lalilu/lmusic/MainApplication.kt
+++ composeApp/src/androidMain/kotlin/com/lalilu/lmusic/MainApplication.kt
@@ -2,9 +2,7 @@ package com.lalilu.lmusic
 
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
-import org.koin.core.logger.Level
 import org.koin.dsl.module
 
 class MainApplication : Application() {
@@ -13,7 +11,6 @@ class MainApplication : Application() {
         startKoin {
             module {
                 androidContext(this@MainApplication)
-                androidLogger(Level.DEBUG)
             }
             koinSetup()
         }
