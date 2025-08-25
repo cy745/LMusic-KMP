64b054ede05385af5b017a0c4f2f9d74a295b1c6
lalilu <1248393538@qq.com>
2025-08-25 08:39:00 +0800

    refactor(lplayer): 为 Android 平台实现 Playback 接口
    - 添加了 PlatformPlayback.android.kt 文件
    - 实现了 Playback 接口的所有方法和属性- 使用了 StateFlow 和 SharedFlow 来管理播放状态和事件- 目前实现的方法大多为空操作或返回默认值，需要进一步开发具体功能

--- lplayer/src/androidMain/kotlin/com/lalilu/lplayer/playback/PlatformPlayback.android.kt
+++ lplayer/src/androidMain/kotlin/com/lalilu/lplayer/playback/PlatformPlayback.android.kt
@@ -1,5 +1,82 @@
 package com.lalilu.lplayer.playback
 
+import com.lalilu.lmedia.entity.LAudio
+import com.lalilu.lmedia.entity.LItem
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+
 actual fun platformPlayback(): Playback {
-    TODO("Not yet implemented")
+    return object : Playback {
+        override fun play() {
+        }
+
+        override fun pause() {
+        }
+
+        override fun togglePlayPause() {
+        }
+
+        override fun stop() {
+        }
+
+        override fun skipTo(index: Int) {
+        }
+
+        override fun skipToNext() {
+        }
+
+        override fun skipTpPrevious() {
+        }
+
+        override fun seekTo(positionMs: Long) {
+        }
+
+        override fun flattenPlaylist(): StateFlow<List<LAudio>> {
+            return MutableStateFlow(emptyList())
+        }
+
+        override fun playlist(): StateFlow<List<LItem>> {
+            return MutableStateFlow(emptyList())
+        }
+
+        override fun updatePlaylist(playlist: List<LItem>) {
+        }
+
+        override fun clearPlaylist() {
+        }
+
+        override fun isPlaying(): StateFlow<Boolean> {
+            return MutableStateFlow(false)
+        }
+
+        override fun currentItem(): StateFlow<LAudio?> {
+            return MutableStateFlow(null)
+        }
+
+        override fun currentItemIndex(): StateFlow<Int> {
+            return MutableStateFlow(0)
+        }
+
+        override fun currentPlaybackState(): StateFlow<PlaybackState> {
+            return MutableStateFlow(PlaybackState.Idle)
+        }
+
+        override fun currentDuration(): Long {
+            return 0L
+        }
+
+        override fun currentPosition(): Long {
+            return 0L
+        }
+
+        override fun currentBufferedPosition(): Long {
+            return 0L
+        }
+
+        override fun errorMessage(): SharedFlow<Throwable> {
+            return MutableSharedFlow()
+        }
+    }
 }
