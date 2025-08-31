89f473f5f387dee3cd96f7b0c990439a7f20d82f
lalilu <1248393538@qq.com>
2025-09-01 01:28:09 +0800

    refactor(lplayer): 重构 LPlayer 类中的实例获取方式
    - 移除了 companion object 中的 instance 属性
    - 使用 KoinPlatform.getKoin().inject<LPlayer>() 替代手动实例管理
    - 简化了类结构，提高了代码的可维护性

--- lplayer/src/commonMain/kotlin/com/lalilu/lplayer/LPlayer.kt
+++ lplayer/src/commonMain/kotlin/com/lalilu/lplayer/LPlayer.kt
@@ -3,18 +3,15 @@ package com.lalilu.lplayer
 import com.lalilu.lplayer.playback.Playback
 import com.lalilu.lplayer.playback.platformPlayback
 import org.koin.core.annotation.Single
+import org.koin.mp.KoinPlatform
 
 
 @Single(createdAtStart = true)
 class LPlayer() : Playback by platformPlayback() {
 
-    init {
-        instance = this
-    }
-
     companion object {
-        lateinit var instance: LPlayer
-            private set
+        val instance: LPlayer by KoinPlatform.getKoin()
+            .inject<LPlayer>()
     }
 }
 
