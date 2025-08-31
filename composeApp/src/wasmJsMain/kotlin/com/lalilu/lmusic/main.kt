5ef5758a8d85e706b50fa8d7eced8284c1d59f11
lalilu <1248393538@qq.com>
2025-09-01 01:29:15 +0800

    feat(composeApp): 加载自定义字体
    - 添加 ReplaceFont组件以替换默认字体
    -预加载 NotoColorEmoji 和 Noto Sans Thin 字体
    - 使用 Compose 的 FontFamily 和 Font API
    - 异步加载字体资源，提高性能

--- composeApp/src/wasmJsMain/kotlin/com/lalilu/lmusic/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/lalilu/lmusic/main.kt
@@ -1,8 +1,21 @@
 package com.lalilu.lmusic
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.platform.LocalFontFamilyResolver
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.platform.Font
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import lmusic_kmp.composeapp.generated.resources.NotoColorEmoji
+import lmusic_kmp.composeapp.generated.resources.NotoSansSC_Regular
+import lmusic_kmp.composeapp.generated.resources.Res
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.getFontResourceBytes
+import org.jetbrains.compose.resources.rememberResourceEnvironment
 import org.koin.core.context.startKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -12,5 +25,26 @@ fun main() {
     ComposeViewport(document.body!!) {
         platformSetupCoil()
         App()
+        ReplaceFont()
+    }
+}
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+fun ReplaceFont() {
+    val fontResolver = LocalFontFamilyResolver.current
+    val resource = rememberResourceEnvironment()
+
+    LaunchedEffect(Unit) {
+        listOf(
+            "NotoColorEmoji" to Res.font.NotoColorEmoji,
+            "Noto Sans Thin" to Res.font.NotoSansSC_Regular
+        ).map {
+            async {
+                val data = getFontResourceBytes(resource, it.second)
+                val font = Font(identity = it.first, data = data)
+                fontResolver.preload(FontFamily(listOf(font)))
+            }
+        }.awaitAll()
     }
 }
