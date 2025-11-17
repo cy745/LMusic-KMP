9e70ff3373ca87e61abbf0756fe104a95d075045
lalilu <1248393538@qq.com>
2025-11-18 02:22:00 +0800

    refactor(common): 简化各平台dispatcherIO实现
    - 移除Android平台不必要的函数包装
    - 移除iOS平台复杂的CPU核心数获取逻辑- 移除JVM平台不必要的函数包装- 统一各平台实现为直接返回Dispatchers.IO
    - 移除iOS平台不再使用的导入和注解

--- common/src/jvmMain/kotlin/com/lalilu/common/ext/DispatchersExtent.jvm.kt
+++ common/src/jvmMain/kotlin/com/lalilu/common/ext/DispatchersExtent.jvm.kt
@@ -3,6 +3,4 @@ package com.lalilu.common.ext
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
-actual fun dispatcherIO(): CoroutineDispatcher {
-    return Dispatchers.IO
-}
+actual fun dispatcherIO(): CoroutineDispatcher = Dispatchers.IO
