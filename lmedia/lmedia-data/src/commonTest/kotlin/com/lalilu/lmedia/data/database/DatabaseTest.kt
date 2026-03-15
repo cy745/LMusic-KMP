86cb7c0e298f5100f11f72267d22e1f96ad22a92
lalilu <1248393538@qq.com>
2026-03-16 01:36:26 +0800

    removal(database): 删除数据库测试相关的多平台实现文件
    - 移除了 androidUnitTest 平台的 DatabaseTest.android.kt 文件
    - 移除了 iosTest 平台的 DatabaseTest.ios.kt 文件
    - 移除了 jvmTest 平台的 DatabaseTest.jvm.kt 文件
    - 移除了 commonTest 平台的 DatabaseTest.kt 文件
    - 移除了 webTest 平台的 DatabaseTest.web.kt 文件

--- lmedia/lmedia-data/src/commonTest/kotlin/com/lalilu/lmedia/data/database/DatabaseTest.kt
+++
@@ -1,8 +0,0 @@
-package com.lalilu.lmedia.data.database
-
-import androidx.room3.RoomDatabase
-
-expect inline fun <reified T : RoomDatabase> requireDatabase(
-    name: String = T::class.qualifiedName!!,
-    forceMemory: Boolean = true
-): T
