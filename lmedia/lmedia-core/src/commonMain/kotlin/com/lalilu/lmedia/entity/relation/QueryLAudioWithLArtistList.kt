a4574b2e85b78bdd3b467d4e93277c8fdc2b8519
lalilu <1248393538@qq.com>
2026-03-16 17:04:42 +0800

    fix(lmedia): 完善 QueryLAudioWithLArtistList 关联查询实体
    - 添加 @Embedded @Relation 注解
    - 实现 LAudio 与 LArtist 的 Room 关联查询

--- lmedia/lmedia-core/src/commonMain/kotlin/com/lalilu/lmedia/entity/relation/QueryLAudioWithLArtistList.kt
+++ lmedia/lmedia-core/src/commonMain/kotlin/com/lalilu/lmedia/entity/relation/QueryLAudioWithLArtistList.kt
@@ -1,4 +1,18 @@
 package com.lalilu.lmedia.entity.relation
 
-class QueryLAudioWithLArtistList {
-}
+import androidx.room3.Embedded
+import androidx.room3.Junction
+import androidx.room3.Relation
+import com.lalilu.lmedia.entity.LArtist
+import com.lalilu.lmedia.entity.LAudio
+
+
+data class QueryLAudioWithLArtistList(
+    @Embedded val audio: LAudio,
+    @Relation(
+        parentColumn = "song_id",
+        entityColumn = "artist_id",
+        associateBy = Junction(CrossRefLAudioXLArtist::class)
+    )
+    val artist: List<LArtist>
+)
